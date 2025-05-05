fe32ef204cebdb97944ec2f193b5a5c9bc722e50
Rerigan <cosnnum@gmail.com>
2025-05-05 00:31:38 -0300

    a

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  output: 'export', // necessário para exportação estática
-  basePath: '/londrijob', // substitua pelo nome do seu repo
-  assetPrefix: isProd ? '/londrijob/' : '',
-};
-
-module.exports = nextConfig;
